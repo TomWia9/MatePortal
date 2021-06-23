a70ec6737042471e084988dc16fab76ac8806c1b
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-06-23 15:43:25 +0200

    Feature/logger (#2)
    * Add serilog packages
    
    * Setup serilog logger

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Api
 {
@@ -31,6 +32,7 @@ namespace Api
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/MatePortalSepcification/swagger.json", "MatePortal"));
+                app.UseSerilogRequestLogging();
             }
 
             app.UseHttpsRedirection();
