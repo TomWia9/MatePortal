7c319d9b584a4d5c634346f3c496b615ecd71cdc
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-06-24 18:05:55 +0200

    Add Roles and Policies classes

---
+++ src/Api/Common/Policies.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Common
+{
+    public static class Policies
+    {
+        public static string UserAccess = "UserAccess";
+        public static string AdminAccess = "AdminAccess";
+    }
+}
---
+++ src/Api/Common/Roles.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Common
+{
+    public static class Roles
+    {
+        public static string User = "User";
+        public static string Administrator = "Administrator";
+    }
+}
