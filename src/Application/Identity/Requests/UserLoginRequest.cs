5cb358078c80b192c97fd2064247d09bfe31b2c4
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-06-24 14:28:32 +0200

    Add auth requests and responses

---
+++ src/Application/Identity/Requests/UserLoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Identity.Requests
+{
+    public class UserLoginRequest
+    {
+        public string Email { get; init; }
+        public string Password { get; init; }
+    }
+}
