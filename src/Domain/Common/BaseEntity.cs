ea144ea2ddaf77a116dcd01f603e7b7cfedfde71
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-06-28 15:50:41 +0200

    Add BaseEntity and DomainEvent abstract classes

---
+++ src/Domain/Common/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public DateTime Created { get; init; }
+        public Guid CreatedBy { get; init; }
+        public DateTime? LastModified { get; set; }
+        public Guid? LastModifiedBy { get; set; }
+    }
+}
