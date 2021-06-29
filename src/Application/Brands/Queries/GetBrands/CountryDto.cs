b319c00e814bdd13531d67d4b3102309469f4d3a
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-06-29 16:41:33 +0200

    Add Brand, Country and YerbaMate dtos

---
+++ src/Application/Brands/Queries/GetBrands/CountryDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Application.Brands.Queries.GetBrands
+{
+    public class CountryDto
+    {
+        public Guid Id { get; init; }
+        public string Name { get; set; }
+    }
+}
