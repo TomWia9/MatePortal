f6e728a76552d2b6eae72c659ca6f96fe8aa09ac
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-06-30 16:29:47 +0200

    Add NotFound, Forbidden and Validation exceptions

---
+++ src/Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Application.Common.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public IDictionary<string, string[]> Errors { get; }
+        
+        public ValidationException() : base("One or more validation failures have occured")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+        
+        //TODO Add second constructor with failures from validator when fluent validation will be added
+    }
+}
