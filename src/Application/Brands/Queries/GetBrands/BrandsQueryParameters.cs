d38e2a3cb36bf90caf34b8e69ce98f7005e80c9d
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-07-18 21:24:20 +0200

    Add BrandsQueryParameters

---
+++ src/Application/Brands/Queries/GetBrands/BrandsQueryParameters.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Extensions;
+using Application.Common.QueryParameters;
+
+namespace Application.Brands.Queries.GetBrands
+{
+    public class BrandsQueryParameters : QueryParameters
+    {
+        private string _country;
+
+        public string Country
+        {
+            get => string.IsNullOrEmpty(_country) ? _country : _country.FirstCharToUpper();
+            set => _country = value;
+        }
+    }
+}
