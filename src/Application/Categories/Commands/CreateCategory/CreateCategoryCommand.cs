518f40e70c1834803ef6af4b3b5b597f2ccb21ea
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-07-18 13:17:30 +0200

    Add commands for categories

---
+++ src/Application/Categories/Commands/CreateCategory/CreateCategoryCommand.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Application.Categories.Commands.CreateCategory
+{
+    public class CreateCategoryCommand : IRequest<Category>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/Application/Categories/Commands/DeleteCategory/DeleteCategoryCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MediatR;
+
+namespace Application.Categories.Commands.DeleteCategory
+{
+    public class DeleteCategoryCommand : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+}
