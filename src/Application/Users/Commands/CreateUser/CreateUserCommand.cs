9d6afecc435ed8caadd2f7f297b173c7a941b8fe
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-07-18 13:46:52 +0200

    Add commands for users

---
+++ src/Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.Users.Queries.GetUser;
+using MediatR;
+
+namespace Application.Users.Commands.CreateUser
+{
+    public class CreateUserCommand : IRequest<UserDto>
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ src/Application/Users/Commands/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MediatR;
+
+namespace Application.Users.Commands.UpdateUser
+{
+    public class UpdateUserCommand : IRequest
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
