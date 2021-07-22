a2ae3903317614da36cd1d57e013161f370a3c88
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-07-22 14:55:36 +0200

    Add missing AddRange in PaginatedList

--- src/Application/Common/Models/PaginatedList.cs
+++ src/Application/Common/Models/PaginatedList.cs
@@ -14,6 +14,7 @@ namespace Application.Common.Models
             PageSize = pageSize;
             CurrentPage = pageNumber;
             TotalPages = (int) Math.Ceiling(count / (double) pageSize);
+            AddRange(items);
         }
 
         public int CurrentPage { get; }
@@ -27,7 +28,7 @@ namespace Application.Common.Models
         public static async Task<PaginatedList<T>> ToPaginatedListAsync(IQueryable<T> source, int pageNumber,
             int pageSize)
         {
-            var count = source.Count();
+            var count = await source.CountAsync();
             var items = await source.Skip(pageSize * (pageNumber - 1)).Take(pageSize).ToListAsync();
 
             return new PaginatedList<T>(items, count, pageNumber, pageSize);
