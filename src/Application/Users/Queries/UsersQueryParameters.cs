9f3de969e1b15ead1190aebe210fbe6e3b33ec61
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-05-20 22:17:00 +0200

    Add Role property to UserDto and UserQueryParameters

--- src/Application/Users/Queries/UsersQueryParameters.cs
+++ src/Application/Users/Queries/UsersQueryParameters.cs
@@ -1,4 +1,5 @@
-﻿using Application.Common.QueryParameters;
+﻿using Application.Common.Extensions;
+using Application.Common.QueryParameters;
 
 namespace Application.Users.Queries
 {
@@ -7,5 +8,15 @@ namespace Application.Users.Queries
     /// </summary>
     public class UsersQueryParameters : QueryParameters
     {
+        private readonly string _role;
+        
+        /// <summary>
+        ///     Role parameter that indicates users roles
+        /// </summary>
+        public string Role
+        {
+            get => string.IsNullOrEmpty(_role) ? _role : _role.FirstCharToUpper();
+            init => _role = value;
+        }
     }
 }
