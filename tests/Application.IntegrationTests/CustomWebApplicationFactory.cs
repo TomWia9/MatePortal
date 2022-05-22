9035b52552ac813f673e565dacaaf75776518ffe
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-05-22 11:24:36 +0200

    Add AppConfiguration to CustomWebApplicationFactory.cs

--- tests/Application.IntegrationTests/CustomWebApplicationFactory.cs
+++ tests/Application.IntegrationTests/CustomWebApplicationFactory.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -52,6 +53,16 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
     /// <param name="builder">The builder</param>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.ConfigureAppConfiguration(configurationBuilder =>
+        {
+            var integrationConfig = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json",true, false)
+                .AddEnvironmentVariables()
+                .Build();
+
+            configurationBuilder.AddConfiguration(integrationConfig);
+        });
+        
         builder.ConfigureServices(services =>
         {
             //Replace CurrentUserService
