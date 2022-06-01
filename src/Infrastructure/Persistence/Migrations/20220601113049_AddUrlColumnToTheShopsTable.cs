65293252e28f9f94af1aaff0e73576ba600044bc
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-06-01 13:32:27 +0200

    Add url field to the shop entity and run migration

---
+++ src/Infrastructure/Persistence/Migrations/20220601113049_AddUrlColumnToTheShopsTable.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class AddUrlColumnToTheShopsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Shops",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Shops");
+        }
+    }
+}
