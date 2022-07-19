a3bab99d7173e33e364aac55ff323ae03b78438c
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-07-19 20:58:15 +0200

    Add DeleteYerbaMateImage command and handler

---
+++ src/Application/YerbaMateImages/Commands/DeleteYerbaMateImage/DeleteYerbaMateImageCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MediatR;
+
+namespace Application.YerbaMateImages.Commands.DeleteYerbaMateImage;
+
+/// <summary>
+///     Delete yerba mate image command
+/// </summary>
+public class DeleteYerbaMateImageCommand: IRequest
+{
+    /// <summary>
+    ///     Yerba mate image ID
+    /// </summary>
+    public Guid YerbaMateImageId { get; init; }
+}
---
+++ src/Application/YerbaMateImages/Commands/DeleteYerbaMateImage/DeleteYerbaMateImageHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.YerbaMateImages.Commands.DeleteYerbaMateImage;
+
+public class DeleteYerbaMateImageHandler : IRequestHandler<DeleteYerbaMateImageCommand>
+{
+    /// <summary>
+    ///     Database context
+    /// </summary>
+    private readonly IApplicationDbContext _context;
+
+    /// <summary>
+    ///     Initializes DeleteOpinionHandler
+    /// </summary>
+    /// <param name="context">Database context</param>
+    public DeleteYerbaMateImageHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    /// <summary>
+    ///     Handles deleting yerba mate image
+    /// </summary>
+    /// <param name="request">Delete yerba mate image request</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <exception cref="NotFoundException">Thrown when yerba mate image is not found</exception>
+    public async Task<Unit> Handle(DeleteYerbaMateImageCommand request, CancellationToken cancellationToken)
+    {
+        var entity = await _context.YerbaMateImages.FindAsync(request.YerbaMateImageId);
+
+        if (entity == null) throw new NotFoundException(nameof(YerbaMateImage), request.YerbaMateImageId);
+
+        _context.YerbaMateImages.Remove(entity);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
