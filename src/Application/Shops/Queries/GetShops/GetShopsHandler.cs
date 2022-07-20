2082bb2c06185cef2c5c5d6a6215977801ac0938
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-07-20 16:44:53 +0200

    Apply new solution in GetShopsHandler

--- src/Application/Shops/Queries/GetShops/GetShopsHandler.cs
+++ src/Application/Shops/Queries/GetShops/GetShopsHandler.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Common.Enums;
 using Application.Common.Interfaces;
 using Application.Common.Mappings;
 using Application.Common.Models;
@@ -31,23 +32,23 @@ public class GetShopsHandler : IRequestHandler<GetShopsQuery, PaginatedList<Shop
     private readonly IMapper _mapper;
 
     /// <summary>
-    ///     Sort service
+    ///     Query service
     /// </summary>
-    private readonly ISortService<Shop> _sortService;
+    private readonly IQueryService<Shop> _queryService;
 
     /// <summary>
     ///     Initializes GetShopsHandler
     /// </summary>
     /// <param name="context">Database context</param>
     /// <param name="mapper">The mapper</param>
-    /// <param name="sortService">Sort service</param>
+    /// <param name="queryService">Query service</param>
     public GetShopsHandler(IApplicationDbContext context,
         IMapper mapper,
-        ISortService<Shop> sortService)
+        IQueryService<Shop> queryService)
     {
         _context = context;
         _mapper = mapper;
-        _sortService = sortService;
+        _queryService = queryService;
     }
 
     /// <summary>
@@ -63,35 +64,73 @@ public class GetShopsHandler : IRequestHandler<GetShopsQuery, PaginatedList<Shop
 
         var collection = _context.Shops.Include(s => s.Opinions) as IQueryable<Shop>;
 
-        //searching
-        if (!string.IsNullOrWhiteSpace(request.Parameters.SearchQuery))
-        {
-            var searchQuery = request.Parameters.SearchQuery.Trim().ToLower();
+        var predicates = GetPredicates(request.Parameters);
 
-            collection = collection.Where(s => s.Name.ToLower().Contains(searchQuery)
-                                               || s.Description.ToLower().Contains(searchQuery)
-                                               || s.Url.ToLower().Contains(searchQuery));
-        }
-
-        //sorting
-        if (!string.IsNullOrWhiteSpace(request.Parameters.SortBy))
-        {
-            var sortingColumns = new Dictionary<string, Expression<Func<Shop, object>>>
-            {
-                {nameof(Shop.Name).ToLower(), s => s.Name},
-                {nameof(Shop.Description).ToLower(), s => s.Description},
-                {nameof(Shop.Opinions).ToLower(), y => y.Opinions.Count}
-            };
-
-            collection = _sortService.Sort(collection, request.Parameters.SortBy,
-                request.Parameters.SortDirection, sortingColumns);
-        }
-        else
-        {
-            collection = collection.OrderBy(b => b.Name);
-        }
+        collection = _queryService.Search(collection, predicates);
+        collection = Sort(collection, request.Parameters.SortBy, request.Parameters.SortDirection);
 
         return await collection.ProjectTo<ShopDto>(_mapper.ConfigurationProvider)
             .PaginatedListAsync(request.Parameters.PageNumber, request.Parameters.PageSize);
+    }
+
+    /// <summary>
+    ///     Gets filtering and searching predicates for shops
+    /// </summary>
+    /// <param name="parameters">The shops query parameters</param>
+    /// <returns>The shops predicates</returns>
+    private static IEnumerable<Expression<Func<Shop, bool>>> GetPredicates(ShopsQueryParameters parameters)
+    {
+        var predicates = new List<Expression<Func<Shop, bool>>>();
+
+        if (string.IsNullOrWhiteSpace(parameters.SearchQuery))
+            return predicates;
+
+        var searchQuery = parameters.SearchQuery.Trim().ToLower();
+
+        predicates.Add(x => x.Name.ToLower().Contains(searchQuery));
+        predicates.Add(x => x.Description.ToLower().Contains(searchQuery));
+        predicates.Add(x => x.Url.ToLower().Contains(searchQuery));
+
+        return predicates.Where(x => x != null);
+    }
+
+    /// <summary>
+    ///     Gets sorting column expression
+    /// </summary>
+    /// <param name="sortBy">Column by which to sort</param>
+    /// <returns>The sorting expression</returns>
+    private static Expression<Func<Shop, object>> GetSortingColumn(string sortBy)
+    {
+        var sortingColumns = new Dictionary<string, Expression<Func<Shop, object>>>
+        {
+            {nameof(Shop.Name).ToLower(), x => x.Name},
+            {nameof(Shop.Description).ToLower(), x => x.Description},
+            {nameof(Shop.Opinions).ToLower(), x => x.Opinions.Count}
+        };
+
+        return sortingColumns[sortBy];
+    }
+
+    /// <summary>
+    ///     Sorts shops by given column in given direction
+    /// </summary>
+    /// <param name="collection">The shops collection</param>
+    /// <param name="sortBy">Column by which to sort</param>
+    /// <param name="sortDirection">Direction in which to sort</param>
+    /// <returns>The sorted collection of shops</returns>
+    private IQueryable<Shop> Sort(IQueryable<Shop> collection, string sortBy, SortDirection sortDirection)
+    {
+        if (!string.IsNullOrWhiteSpace(sortBy))
+        {
+            var sortingColumn = GetSortingColumn(sortBy);
+
+            collection = _queryService.Sort(collection, sortingColumn, sortDirection);
+        }
+        else
+        {
+            collection = collection.OrderBy(x => x.Name);
+        }
+
+        return collection;
     }
 }
