72c8fd06f9f1a794cabfa46a3effadd1fd408f09
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-07-23 17:07:18 +0200

    Add BadRequestException

---
+++ src/Application/Common/Exceptions/BadRequestException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Application.Common.Exceptions;
+
+/// <summary>
+///     Bad request exception
+/// </summary>
+public class BadRequestException : Exception
+{
+    /// <summary>
+    ///     Initializes default BadRequestException
+    /// </summary>
+    public BadRequestException()
+    {
+    }
+
+    /// <summary>
+    ///     Initializes BadRequestException with message
+    /// </summary>
+    /// <param name="message">The message</param>
+    public BadRequestException(string message) : base(message)
+    {
+    }
+}
