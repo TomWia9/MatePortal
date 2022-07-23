5ec3a8e9092d9fdd4de64415b022431de33c0fe3
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-07-23 17:47:19 +0200

    Add ApiControllerBase to IdentityController

--- src/Api/Controllers/IdentityController.cs
+++ src/Api/Controllers/IdentityController.cs
@@ -2,7 +2,6 @@
 using Application.Users.Commands.LoginUser;
 using Application.Users.Commands.RegisterUser;
 using Application.Users.Responses;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
@@ -11,33 +10,19 @@ namespace Api.Controllers;
 ///     The identity controller
 /// </summary>
 [Route("api/[controller]")]
-public class IdentityController : ControllerBase
+public class IdentityController : ApiControllerBase
 {
-    /// <summary>
-    ///     The mediator
-    /// </summary>
-    private readonly IMediator _mediator;
-
-    /// <summary>
-    ///     Initializes IdentityController
-    /// </summary>
-    /// <param name="mediator">The mediator</param>
-    public IdentityController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
     /// <summary>
     ///     Registers the user
     /// </summary>
     /// <param name="request">The register request</param>
     /// <returns>An IActionResult</returns>
     /// <response code="200">Creates user and returns jwt token</response>
-    /// <response code="400">Creates user and returns jwt token</response>
+    /// <response code="400">Bad request</response>
     [HttpPost("register")]
     public async Task<IActionResult> Register([FromBody] RegisterUserCommand request)
     {
-        var result = await _mediator.Send(request);
+        var result = await Mediator.Send(request);
 
         if (!result.Success)
             return BadRequest(new AuthFailedResponse
@@ -57,10 +42,11 @@ public class IdentityController : ControllerBase
     /// <param name="request">The user login request</param>
     /// <returns>An IActionResult</returns>
     /// <response code="200">Authenticates user and returns jwt token</response>
+    /// <response code="400">Bad request</response>
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] LoginUserCommand request)
     {
-        var result = await _mediator.Send(request);
+        var result = await Mediator.Send(request);
 
         if (!result.Success)
             return BadRequest(new AuthFailedResponse
