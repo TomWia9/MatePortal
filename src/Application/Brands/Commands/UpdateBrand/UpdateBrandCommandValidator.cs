723f00ec915f9f1e36f4343641f5c05d85277373
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-10-25 11:17:56 +0200

    Add UpdateBrandCommandValidator

---
+++ src/Application/Brands/Commands/UpdateBrand/UpdateBrandCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Application.Brands.Commands.UpdateBrand;
+
+/// <summary>
+///     UpdateBrandCommand validator
+/// </summary>
+public class UpdateBrandCommandValidator : AbstractValidator<UpdateBrandCommand>
+{
+    /// <summary>
+    ///     Initializes CreateBrandCommandValidator
+    /// </summary>
+    public UpdateBrandCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .MaximumLength(60)
+            .NotEmpty();
+
+        RuleFor(x => x.Description)
+            .MaximumLength(1000)
+            .NotEmpty();
+    }
+}
