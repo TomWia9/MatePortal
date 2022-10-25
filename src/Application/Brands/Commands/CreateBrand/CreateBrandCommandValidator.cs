2018e9d8ed0d80e0c6360a9247f640d8ff165325
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-10-25 11:17:46 +0200

    Add CreateBrandCommandValidator

---
+++ src/Application/Brands/Commands/CreateBrand/CreateBrandCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Application.Brands.Commands.CreateBrand;
+
+/// <summary>
+///     CreateBrandCommand validator
+/// </summary>
+public class CreateBrandCommandValidator : AbstractValidator<CreateBrandCommand>
+{
+    /// <summary>
+    ///     Initializes CreateBrandCommandValidator
+    /// </summary>
+    public CreateBrandCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .MaximumLength(60)
+            .NotEmpty();
+
+        RuleFor(x => x.Description)
+            .MaximumLength(1000)
+            .NotEmpty();
+    }
+}
