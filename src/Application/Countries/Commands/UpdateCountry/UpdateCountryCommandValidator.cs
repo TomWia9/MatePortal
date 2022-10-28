28c2d897041f47cebf6c36b90bbbeb9fee849254
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-10-28 09:04:58 +0200

    Add UpdateCountryCommandValidator

---
+++ src/Application/Countries/Commands/UpdateCountry/UpdateCountryCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Application.Countries.Commands.UpdateCountry;
+
+/// <summary>
+///     UpdateCountryCommand validator
+/// </summary>
+public class UpdateCountryCommandValidator : AbstractValidator<UpdateCountryCommand>
+{
+    /// <summary>
+    ///     Initializes UpdateCountryCommandValidator
+    /// </summary>
+    public UpdateCountryCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().MaximumLength(50);
+
+        RuleFor(x => x.Description)
+            .NotEmpty().MaximumLength(1000);
+    }
+}
