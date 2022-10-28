67c0525eb09753b808d113666013530d213273f0
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-10-28 19:08:40 +0200

    Add GetShopOpinionsQueryValidator

---
+++ src/Application/ShopOpinions/Queries/GetShopOpinions/GetShopOpinionsQueryValidator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Domain.Entities;
+using FluentValidation;
+
+namespace Application.ShopOpinions.Queries.GetShopOpinions;
+
+/// <summary>
+///     GetShopOpinionsQuery validator
+/// </summary>
+public class GetShopOpinionsQueryValidator : AbstractValidator<ShopOpinionsQueryParameters>
+{
+    /// <summary>
+    ///     The columns allowed to sort by
+    /// </summary>
+    private readonly IEnumerable<string> _sortingColumns = new List<string>
+    {
+        nameof(ShopOpinion.Rate).ToLower(),
+        nameof(ShopOpinion.Comment).ToLower(),
+        nameof(ShopOpinion.Created).ToLower()
+    };
+
+    /// <summary>
+    ///     GetShopOpinionsQueryValidator
+    /// </summary>
+    public GetShopOpinionsQueryValidator()
+    {
+        RuleFor(x => x.SortBy)
+            .Must(value =>
+                string.IsNullOrEmpty(value) || _sortingColumns.Contains(value.ToLower()))
+            .WithMessage($"SortBy must be in [{string.Join(", ", _sortingColumns)}]");
+
+        RuleFor(x => x.MinRate)
+            .GreaterThanOrEqualTo(1)
+            .LessThanOrEqualTo(10);
+
+        RuleFor(x => x.MaxRate)
+            .GreaterThanOrEqualTo(1)
+            .LessThanOrEqualTo(10);
+    }
+}
