564e9840735345dae308798eb9d0570bf8f6ccf2
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-10-28 09:33:21 +0200

    Add UpdateShopCommandValidator

---
+++ src/Application/Shops/Commands/UpdateShop/UpdateShopCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FluentValidation;
+
+namespace Application.Shops.Commands.UpdateShop;
+
+/// <summary>
+///     UpdateShopCommand validator
+/// </summary>
+public class UpdateShopCommandValidator : AbstractValidator<UpdateShopCommand>
+{
+    /// <summary>
+    ///     Initializes UpdateShopCommandValidator
+    /// </summary>
+    public UpdateShopCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.Description)
+            .NotEmpty()
+            .MaximumLength(1000);
+
+        RuleFor(x => x.Url)
+            .NotEmpty()
+            .MaximumLength(200)
+            .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _))
+            .WithMessage("The url field has an invalid format.");
+    }
+}
