78d2038770fddec3862404ace4d536c9efc301f0
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-10-28 08:57:19 +0200

    Feature/categories-validation (#39)
    * Add CreateCategoryCommandValidator
    
    * Add UpdateCategoryCommandValidator
    
    * Add GetCategoriesQueryValidator

---
+++ src/Application/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Application.Categories.Commands.CreateCategory;
+
+/// <summary>
+///     CreateCategoryCommand validator
+/// </summary>
+public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+{
+    /// <summary>
+    ///     Initializes CreateCategoryCommandValidator
+    /// </summary>
+    public CreateCategoryCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().MaximumLength(50);
+
+        RuleFor(x => x.Description)
+            .NotEmpty().MaximumLength(1000);
+    }
+}
---
+++ src/Application/Categories/Queries/GetCategories/GetCategoriesQueryValidator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Domain.Entities;
+using FluentValidation;
+
+namespace Application.Categories.Queries.GetCategories;
+
+/// <summary>
+///     GetCategoriesQuery validator
+/// </summary>
+public class GetCategoriesQueryValidator : AbstractValidator<CategoriesQueryParameters>
+{
+    private readonly IEnumerable<string> _sortingColumns = new List<string>
+    {
+        nameof(Category.Name).ToLower(),
+        nameof(Category.Description).ToLower()
+    };
+    
+    /// <summary>
+    ///     Initializes GetCategoriesQueryValidator
+    /// </summary>
+    public GetCategoriesQueryValidator()
+    {
+        RuleFor(x => x.SortBy)
+            .Must(value =>
+                string.IsNullOrEmpty(value) || _sortingColumns.Contains(value.ToLower()))
+            .WithMessage($"SortBy must be in [{string.Join(", ", _sortingColumns)}]");
+    }
+}
