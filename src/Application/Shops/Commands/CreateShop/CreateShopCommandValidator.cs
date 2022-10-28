b0a86daa34fe3cb0935fc47c5b29989b90fdca4d
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-10-28 09:33:14 +0200

    Add CreateShopCommandValidator

---
+++ src/Application/Shops/Commands/CreateShop/CreateShopCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FluentValidation;
+
+namespace Application.Shops.Commands.CreateShop;
+
+/// <summary>
+///     CreateShopCommand validator
+/// </summary>
+public class CreateShopCommandValidator : AbstractValidator<CreateShopCommand>
+{
+    /// <summary>
+    ///     Initializes CreateShopCommandValidator
+    /// </summary>
+    public CreateShopCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.Description)
+            .NotEmpty()
+            .MaximumLength(1000);
+        
+        RuleFor(x => x.Url)
+            .NotEmpty()
+            .MaximumLength(200)
+            .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _))
+            .WithMessage("The url field has an invalid format.");
+    }
+}
