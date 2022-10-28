3555bd538a4c943fd208e6af46c060452d39ba61
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-10-28 19:15:38 +0200

    Feature/shop-opinions-validation (#42)
    * Add CreateShopOpinionCommandValidator
    
    * Add GetShopOpinionsQueryValidator
    
    * Add UpdateShopOpinionCommandValidator

---
+++ src/Application/ShopOpinions/Commands/UpdateShopOpinion/UpdateShopOpinionCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Application.ShopOpinions.Commands.UpdateShopOpinion;
+
+/// <summary>
+///     UpdateShopOpinionCommand validator
+/// </summary>
+public class UpdateShopOpinionCommandValidator : AbstractValidator<UpdateShopOpinionCommand>
+{
+    /// <summary>
+    ///     Initializes UpdateShopOpinionCommandValidator
+    /// </summary>
+    public UpdateShopOpinionCommandValidator()
+    {
+        RuleFor(x => x.Rate)
+            .NotEmpty()
+            .InclusiveBetween(1, 10);
+
+        RuleFor(x => x.Comment)
+            .NotEmpty()
+            .MaximumLength(500);
+    }
+}
