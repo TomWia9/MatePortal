d2494f1b942f547e1a3141c4e0a547cfd3ff277c
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-11-12 18:04:31 +0100

    Add validators for commands

---
+++ src/Application/Users/Commands/DeleteUser/DeleteUserCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Commands.DeleteUser;
+
+/// <summary>
+///     DeleteUserCommand validator
+/// </summary>
+public class DeleteUserCommandValidator : AbstractValidator<DeleteUserCommand>
+{
+    /// <summary>
+    ///     Initializes DeleteUserCommandValidator
+    /// </summary>
+    public DeleteUserCommandValidator()
+    {
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MaximumLength(256);
+    }
+}
---
+++ src/Application/Users/Commands/UpdateUser/UpdateUserCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace Application.Users.Commands.UpdateUser;
+
+/// <summary>
+///     UpdateUserCommand validator
+/// </summary>
+public class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
+{
+    /// <summary>
+    ///     Initializes UpdateUserCommandValidator
+    /// </summary>
+    public UpdateUserCommandValidator()
+    {
+        RuleFor(x => x.Username)
+            .NotEmpty()
+            .MaximumLength(256);
+
+        RuleFor(x => x.CurrentPassword)
+            .NotEmpty()
+            .MaximumLength(256);
+
+        RuleFor(x => x.NewPassword)
+            .NotEmpty()
+            .NotEqual(x => x.CurrentPassword)
+            .MaximumLength(256);
+    }
+}
