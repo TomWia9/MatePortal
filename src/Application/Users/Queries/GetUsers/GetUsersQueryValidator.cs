0d5ce9e3775325ca03d5ec17c0189b09691d1359
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-11-12 18:23:59 +0100

    Add GetUsersQueryValidator

---
+++ src/Application/Users/Queries/GetUsers/GetUsersQueryValidator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentValidation;
+
+namespace Application.Users.Queries.GetUsers;
+
+/// <summary>
+///     GetUsersQuery validator
+/// </summary>
+public class GetUsersQueryValidator : AbstractValidator<UsersQueryParameters>
+{
+    /// <summary>
+    ///     The columns allowed to sort by
+    /// </summary>
+    private readonly IEnumerable<string> _sortingColumns = new List<string>
+    {
+        "email",
+        "username",
+        "yerbamateopinions",
+        "shopopinions"
+    };
+
+    /// <summary>
+    ///     GetUsersQueryValidator
+    /// </summary>
+    public GetUsersQueryValidator()
+    {
+        RuleFor(x => x.Role)
+            .MaximumLength(15);
+
+        RuleFor(x => x.SortBy)
+            .Must(value =>
+                string.IsNullOrEmpty(value) || _sortingColumns.Contains(value.ToLower()))
+            .WithMessage($"SortBy must be in [{string.Join(", ", _sortingColumns)}]");
+    }
+}
