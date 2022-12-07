1d05bac3c892af592704a5180bf1e1cb0b4ec809
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2022-12-07 10:37:51 +0100

    Add QueryValidator

---
+++ src/Application/Common/QueryParameters/QueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Application.Common.QueryParameters;
+
+/// <summary>
+///     Query validator
+/// </summary>
+public class QueryValidator<T> : AbstractValidator<T> where T : QueryParameters
+{
+    /// <summary>
+    ///     Initializes QueryValidator
+    /// </summary>
+    public QueryValidator()
+    {
+        RuleFor(x => x.PageNumber).GreaterThanOrEqualTo(1);
+        RuleFor(x => x.PageSize).GreaterThanOrEqualTo(1);
+        RuleFor(x => x.SearchQuery).MaximumLength(100);
+        RuleFor(x => x.SortDirection).IsInEnum();
+    }
+}
